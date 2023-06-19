73c48aaedec6d6fb8ff243507764e89e37825aab
Pedro Freitas <fjpf.pf@gmail.com>
2023-06-19 22:13:05 +0100

    menu mais completo

--- menu_master_0.5.ino
+++ menu_master_0.5.ino
@@ -12,6 +12,7 @@
 #define MAGENTA 0xF81F
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
+#define DARKER_GREEN 0x0706
 
 namespace
 {
@@ -19,20 +20,81 @@ namespace
   Adafruit_GFX &tft = Waveshield;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  SPI.begin();
-  Waveshield.begin();
-  tft.fillScreen(WHITE);
-  tft.setRotation(3);
-  tft.fillRoundRect(200, 50, 40, 40, 3, GREEN);
-  tft.setTextColor(WHITE); // Define a cor do texto para branco
-  tft.setTextSize(1); // Define o tamanho do texto para 2
-  tft.setCursor(205, 60); // Define a posição do texto dentro do botão
-  tft.println("Botao"); // Escreve o texto no botão
+void drawHomeScreen()
+{
+  tft.setTextSize(3);
+  tft.setTextColor(BLUE);
 
+  // ALINHA E IMPRIME "MENU PRINCIPAL" 
+  int16_t textWidth, textHeight;
+  tft.getTextBounds("MENU PRINCIPAL", 0, 0, nullptr, nullptr, &textWidth, &textHeight);
+
+  int16_t textX = (tft.width() - textWidth) / 2; // Centraliza o texto horizontalmente
+  int16_t textY = 15; // Posição vertical do texto
+
+  tft.setCursor(textX, textY);
+  tft.print("MENU PRINCIPAL");
+  //-------------------------------
+
+  // LINHA HORIZONTAL
+  int16_t x = 0;       // coordenada x do ponto inicial
+  int16_t y = 50;      // coordenada y do ponto inicial
+  int16_t width = 480;  // largura da linha
+  int16_t thickness = 3; // espessura da linha
+  uint16_t color = BLUE; // cor da linha
+
+  for (int16_t i = 0; i < thickness; i++) {
+    tft.drawFastHLine(x, y + i, width, color);
+  }
+  //-------------------------------
+
+  int buttonWidth = 200;
+  int buttonHeight = 50;
+  int screenWidth = tft.width();
+  int screenHeight = tft.height();
+  int buttonX = (screenWidth - buttonWidth) / 2;
+  int buttonY = (screenHeight - buttonHeight) / 2 - 60;
+
+  // BUTÃO INCIAR PROGRAMA / ENCERRAR PROGRAMA
+  tft.fillRoundRect(buttonX, buttonY, buttonWidth, buttonHeight, 10, DARKER_GREEN);
+  tft.setTextColor(WHITE); // Define a cor do texto para branco
+  tft.setTextSize(3); // Define o tamanho do texto para 3
+  tft.setCursor(buttonX + (buttonWidth - 30) / 4, buttonY + (buttonHeight - textHeight) / 2);
+  tft.println("INICIAR"); // Escreve o texto no botão
+
+  // BOTÃO AJUSTAR VALVULAS
+  int ajustesButtonY = buttonY + buttonHeight + 20;
+  tft.fillRoundRect(buttonX, ajustesButtonY, buttonWidth, buttonHeight, 10, BLUE);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(buttonX + (buttonWidth - 50) / 4, ajustesButtonY + (buttonHeight - 8) / 3);
+  tft.println("VALVULAS");
+
+  // Informação das válvulas ativas
+  int valveInfoWidth = 80;
+  int valveInfoHeight = 30;
+  int valveInfoX = tft.width() - valveInfoWidth - 10; // Posição X 
+  int valveInfoY = 280; // Posição Y 
+  int valveInfoSpacing = 5; // Espaçamento entre os retângulos das válvulas ativas
+
+  tft.fillRoundRect(valveInfoX, valveInfoY, valveInfoWidth, valveInfoHeight, 5, GREEN);
+  tft.setTextColor(WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(valveInfoX + valveInfoHeight + valveInfoSpacing, valveInfoY + valveInfoHeight / 2);
+  tft.println("VALVULA: 0");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() 
+{
+  SPI.begin();
+  Waveshield.begin();
+  tft.fillScreen(BLACK);
+  tft.setRotation(3);
+
+  drawHomeScreen();
+}
+
+void loop() 
+{
+  
 }
